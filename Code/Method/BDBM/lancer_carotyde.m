7e0498911e6bcd123f57ea6e73bea84024bd1a59
ZaOniRinku <ZaOniRinku@users.noreply.github.com>
2020-02-09 13:37:52 +0100

    Add files via upload

--- Code/Method/BDBM/lancer_carotyde.m
+++ Code/Method/BDBM/lancer_carotyde.m
@@ -1,8 +1,12 @@
 clear; close all; clc
 
-Z_im=[ 50  160 ;   10   140]; % ROI
-Grid=[3 3]; % distance between two pixels to estimate
-L=[10 10]; % block size
+h_min = 150;
+h_max = 300;
+w_min = 400;
+w_max = 700;
+Z_im=[ h_min  h_max ;   w_min   w_max]; % ROI
+Grid=[10 10]; % distance between two pixels to estimate
+L=[30 30]; % block size
 vI=[1 1]; % interpolation factor of search zones
 nb_it=1;
 dP_ini_limit = [0 0 ; 0 0];
@@ -13,10 +17,10 @@ type_interp = 1;
 f = [0.13 0.13];
 US_IRM = 0;
 
-load imTaggedMRI.mat
-im = double(im(50:250,1:160,:));
+load IM_0031.mat
+im = double(im(1:600,1:800,:));
 
-debut = 1; pas = 1; fin = 11;
+debut = 1; pas = 25; fin = 900;
 
 % For motion vector plot
 pas_x = 5; pas_y = 5;
@@ -26,7 +30,7 @@ for i = debut:pas:fin
    
     image(:,:,1)=im(:,:,i); image(:,:,2)=im(:,:,i+pas);
     
-    [chp_dep_est x_grid y_grid]=bdbm(image,Z_im,L,Grid,vI,nb_it,type_correl,type_calcul_dep,dP_ini_limit,tR,f,US_IRM);   
+    [chp_dep_est,x_grid,y_grid]=bdbm(image,Z_im,L,Grid,vI,nb_it,type_correl,type_calcul_dep,dP_ini_limit,tR,f,US_IRM);   
     [chp_dep_int,pts_ax,pts_lat] = denseField(chp_dep_est,x_grid, y_grid,Grid,type_interp);
 
     if i==debut
@@ -56,7 +60,7 @@ for i = debut:pas:fin
 end
 
 figure;
-J1 = DeformMesh(im(40:160,20:120,:),[5 5],18:90,28:100,axial_motion,lateral_motion,x_grid-20,y_grid-40,[2 2])
+J1 = DeformMesh(im(h_min:h_max,w_min:w_max,:),[10 10],18:90,28:100,axial_motion,lateral_motion,x_grid-20,y_grid-40,[2 2])
 movie(J1,100,5)
 % J1 = DeformMesh45d(im(100:200,20:110,:),[3 3],10:90,20:90,axial_motion,lateral_motion,x_grid-20,y_grid-100,[2 2])
 
