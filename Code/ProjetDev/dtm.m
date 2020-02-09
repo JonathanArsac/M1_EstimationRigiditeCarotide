23c433e8c1e5e3db28372865ccb2a97ad9dda536
ZaOniRinku <ZaOniRinku@users.noreply.github.com>
2020-02-09 12:02:57 +0100

    Create dtm.m

---
+++ Code/ProjetDev/dtm.m
@@ -0,0 +1,13 @@
+% Save DICOM file (4D) to .mat file (3D)
+
+% read DICOM
+dicomfile = 'IM_0031';
+dicomdir = '';
+[im, map] = dicomread(fullfile(dicomdir, dicomfile));
+disp(size(im));
+im = squeeze(im(:,:,1,:));
+
+% write to mat
+matfile = append(dicomfile, '.mat');
+matdir = '';
+save(fullfile(matdir, matfile), 'im', 'map');
